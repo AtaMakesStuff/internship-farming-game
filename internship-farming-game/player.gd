faa511311fc9fdb2e24129b3221094a80a496b25
AtaMakesStuff <sinankit@udel.edu>
2025-09-02 13:37:43 -0400

    Player movement
    - disabled gravity and jumping for now
    - added vertical movement

--- internship-farming-game/player.gd
+++ internship-farming-game/player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# I don't currently want gravity
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
 
 	# Handle jump.
 	# I don't currently want jumping
@@ -17,10 +18,16 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontal_direction := Input.get_axis("left", "right")
+	if horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var vertical_direction := Input.get_axis("up", "down")
+	if vertical_direction:
+		velocity.y = vertical_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
