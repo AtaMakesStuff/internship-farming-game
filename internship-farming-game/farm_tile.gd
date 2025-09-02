31194ff76625071474398aa12d8eb0f5f5c89a72
AtaMakesStuff <sinankit@udel.edu>
2025-09-02 14:03:59 -0400

    Farm Tile wip
    - Made images for farm tile
    - Can click on tile and till it but only on center

---
+++ internship-farming-game/farm_tile.gd
@@ -0,0 +1,26 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var tex = $ColorRect.texture
+		if tex == null:
+			return
+		
+		var sprite_size = tex.get_size() * scale
+		var top_left = global_position - sprite_size / 2.0
+		var mouse_pos = get_global_mouse_position()
+		var sprite_rect = Rect2(top_left, sprite_size)
+		
+		if sprite_rect.has_point(mouse_pos):
+			$ColorRect.hide()
+			$ColorRect2.show()
